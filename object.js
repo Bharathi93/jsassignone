dee6aae2f985f779e2fe8d39c887450a374404ba
Bharathi <bharathi.v21@wipro.com>
2016-08-30 17:20:05 +0530

    Changes saved

--- object.js
+++ object.js
@@ -12,7 +12,7 @@ console.log("Display object based on value..")
   for(var i=0;i<6;i++)
   {
   if(myObj[i].occupation=="Programmer")
-  //a();
+
   console.log(myObj[i].name+" "+myObj[i].age);
   }
 
@@ -27,6 +27,24 @@ for(var i=0;i<6;i++)
 {
 console.log(myObj[i].name+" "+myObj[i].age+" "+myObj[i].occupation);
 }
-
-//if(myObj.value="Programmer")
-//console.log(myObj.key);
+//Recreate the objects//
+console.log("Recreate the objects..");
+reArrange();
+function reArrange()
+{
+  var Array1=[];
+  var Array2 =[];
+  for(i =0;i<myObj.length;i++)
+  {
+    if(myObj[i].occupation === "Team Leader")
+    {
+      Array1.push({name:myObj[i].name,age:myObj[i].age});
+    }
+    if(myObj[i].occupation === "Programmer")
+    {
+      Array2.push({name:myObj[i].name,age:myObj[i].age});
+    }
+  }
+  var Obj ={"Team Leader":Array1,"Programmer":Array2}
+  console.log(Obj);
+}
